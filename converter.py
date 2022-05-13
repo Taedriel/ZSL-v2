0afea23c001ecaf06fba46e19f7e389fc9a8e1ab
Ismaël Delorme <I.Delorme@kingston.ac.uk>
2022-05-13 16:41:43 +0100

    print sim mat

--- converter.py
+++ converter.py
@@ -9,8 +9,8 @@ from typing import List
 
 class WordToVecteur:
 
-    def __init__(self, listTags : List[str] = []):
-        self.listTags = listTags
+    def __init__(self, list_tags : List[str] = []):
+        self.list_tags = list_tags
 
     def export(self, filename):
         return NotImplementedError
@@ -24,16 +24,16 @@ class WordToVecteur:
 
 class BERTModel(WordToVecteur):
 
-    def __init__(self, listTag : List[str] = [], big: bool = False):
-        super(BERTModel, self).__init__(listTag)
-        self.modelSize = "bert-large-uncased" if big else "bert-base-uncased"
+    def __init__(self, list_tag : List[str] = [], big: bool = False):
+        super(BERTModel, self).__init__(list_tag)
+        self.model_size = "bert-large-uncased" if big else "bert-base-uncased"
         self.embeddings = []
-        self.cosineSimMatrix = None
+        self.cosine_sim_matrix = None
 
-        self.tokenizer = BertTokenizer.from_pretrained(self.modelSize, padding=True, truncation=True,)
-        self.model = BertModel.from_pretrained(self.modelSize, output_hidden_states = True)
+        self.tokenizer = BertTokenizer.from_pretrained(self.model_size, padding=True, truncation=True,)
+        self.model = BertModel.from_pretrained(self.model_size, output_hidden_states = True)
 
-        self.mergingStrategy = Sum4LastLayers()
+        self.merging_strategy = Sum4LastLayers()
 
         self.model.eval()
 
@@ -52,7 +52,10 @@ class BERTModel(WordToVecteur):
                 line = ",".join(list(map(str, map(float, embedding[1]))))
                 print(embedding[0], ",", line, sep="", file=f)
 
-    def importTagList(self, filename):
+    def import_tag_list(self, filename):
+        self.embeddings.clear()
+        self.cosine_sim_matrix = None
+
         try:
             f = open(filename, "r")
         except OSError:
@@ -61,14 +64,16 @@ class BERTModel(WordToVecteur):
         with f:
             data = f.read().split("\n")
             for item in data:
-                if item not in self.listTags and str.strip(item) != "":
-                    self.listTags.append(item)
-            print(self.listTags)
-            logging.info(f"Import finished : {len(self.listTags)} elements imported.")
+                if item not in self.list_tags and str.strip(item) != "":
+                    self.list_tags.append(item)
+            print(self.list_tags)
+            logging.info(f"Import finished : {len(self.list_tags)} elements imported.")
             
 
     def convert(self):
-        for i, tag in enumerate(self.listTags):
+        """ convert all word in their embeddings"""
+        logging.info()
+        for i, tag in enumerate(self.list_tags):
 
             inputs = self.tokenizer(tag, return_tensors = "pt")
 
@@ -79,7 +84,7 @@ class BERTModel(WordToVecteur):
 
             # log.info(f"[{i}]","Number of layers:", len(hidden_states), "  (initial embeddings + 12 BERT layers)")
             # log.info(f"[{i}]","Number of batches:", len(hidden_states[0]))
-            # log.info(f"[{i}]","Number of tokens:", len(hidden_states[0][0]) - 2)
+            logging.info(f"[{i}]","Number of tokens:", len(hidden_states[0][0]) - 2)
             # log.info(f"[{i}]","Number of hidden units:", len(hidden_states[0][0][0]))
 
             # [# layers, # batches, # tokens, # features] ==> [# tokens, # layers, # features]
@@ -90,49 +95,65 @@ class BERTModel(WordToVecteur):
             # apply different strategy to summarize word embeddings
             tokenized_text = self.tokenizer.tokenize(tag)
             for i, token in enumerate(tokenized_text):
-                embed = self.mergingStrategy.merge(token_embeddings[i+1])
+                embed = self.merging_strategy.merge(token_embeddings[i+1])
                 self.embeddings.append((token, embed))
 
-    def computeCoSim(self):
+    def compute_sim(self):
         """ compute cosine similarity between all vectors """
         if len(self.embeddings) == 0:
             raise Exception("Tags not converted yet !")
 
         logging.info("Computing cosine similarity, this could take some time...")
 
-        nTokens = len(self.embeddings)
-        self.cosineSimMatrix = [[0 for j in range(nTokens)] for i in range(nTokens)]
+        n_tokens = len(self.embeddings)
+        self.cosine_sim_matrix = [[1 for j in range(n_tokens)] for i in range(n_tokens)]
 
         for j, vector in enumerate(self.embeddings):
 
-            for i, otherVector in enumerate(self.embeddings):
+            for i, other_vector in enumerate(self.embeddings):
 
                 if i == j:
                     continue
+
                 
                 cos = torch.nn.CosineSimilarity(dim=0)
-                similarity = cos(vector[1], otherVector[1])
+                similarity = cos(vector[1], other_vector[1])
 
-                self.cosineSimMatrix[i][j] = similarity
-                self.cosineSimMatrix[j][i] = similarity
+                self.cosine_sim_matrix[i][j] = similarity
+                self.cosine_sim_matrix[j][i] = similarity
 
-    def simBetween(self, token1, token2):
-        if self.cosineSimMatrix is None:
-            self.computeCoSim()
+    def export_sim_matrix(self, filename):
+        if self.cosine_sim_matrix == None:
+            self.compute_sim()
+        
+        try:
+            f = open(filename, "w")
+        except OSError:
+            raise OSError("Could not open file")
+
+        with f:
+            print("/", ",".join([tag[0] for tag in self.embeddings]), sep = ",", file = f)
+
+            for j, tag_y in enumerate(self.embeddings):
+                print(tag_y, ",".join([self.cosine_sim_matrix[j][i] for i in range(len(self.embeddings))]), sep = ",", file = f)
+
+    def sim_between(self, token1, token2):
+        if self.cosine_sim_matrix is None:
+            self.compute_co_sim()
 
         index1 = [i for i, v in enumerate(self.embeddings) if v[0] == token1][0]
         index2 = [i for i, v in enumerate(self.embeddings) if v[0] == token2][0]
 
-        return self.cosineSimMatrix[index1][index2]
+        return self.cosine_sim_matrix[index1][index2]
 
-    def getEmbeddingOf(self, token):
+    def get_embedding_of(self, token):
         res = [v for v in self.embeddings if v[0] == token]
         if len(res) == 0:
             raise Exception("no such token")
         
         return res[0]
 
-    def getNearestEmbeddingOf(self, embedding, nb = 10):
+    def get_nearest_embedding_of(self, embedding, nb = 10):
 
         if nb > len(self.embeddings):
             raise Exception("nb too high, not enough token")
