4036bd20fd8b8a58d265060d999dc035d932f95b
Ismaël Delorme <isam.delorme@gmail.com>
2022-08-03 12:26:45 +0100

    adding info to fill later

--- zsl/__init__.py
+++ zsl/__init__.py
@@ -5,9 +5,27 @@ from word_embeddings import *
 from test_word_embeddings import *
 
 import logging
-import warnings
 
 FORMAT = '%(levelname)-10s %(message)s'
 logging.basicConfig(format=FORMAT, level = logging.INFO, filename = "ZSL.log" )
 
-warnings.filterwarnings("ignore", category=UserWarning, module='wikipedia')
+
+NAME = 'ZSL-v2'
+
+__version__ = "0.0.1"
+VERSION = __version__
+
+DESCRIPTION = ""
+
+AUTHOR = ""
+AUTHOR_EMAIL = ""
+URL = ""
+PROJECT_URLS = {
+    'Documentation': "",
+    'Source Code': "",
+    'Issue Tracker': "",
+    'Donate': ""
+}
+LICENSE = ""
+
+KEYWORDS = []
